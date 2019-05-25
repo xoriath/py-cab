737ca399f34dcaed7f7f7e90b6a64de86958f1fc
xoriath <moro.engelhardt@gmail.com>
2019-05-24 22:10:36 -0700

    Add more management

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+      long_description = fh.read()
+
 setup(name='cab',
-      version='0.1',
+      version='0.0.1',
       description='Unpack cab archives',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/xoriath/py-cab',
       author='Morten Engelhardt Olsen',
       author_email='moro.engelhardt@gmail.com',
