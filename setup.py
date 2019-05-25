061bb636d5804e076cd7852aace65236537248fd
xoriath <moro.engelhardt@gmail.com>
2019-05-24 19:05:52 -0700

    Use find_packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cab',
       version='0.1',
@@ -7,5 +7,5 @@ setup(name='cab',
       author='Morten Engelhardt Olsen',
       author_email='moro.engelhardt@gmail.com',
       license='MIT',
-      packages=['cab'],
+      packages=find_packages('cab'),  
       zip_safe=False)
