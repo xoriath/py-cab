e123e89332cf96b2721e2755020079ed4313115d
xoriath <moro.engelhardt@gmail.com>
2019-05-24 21:53:35 -0700

    Update classifiers

--- setup.py
+++ setup.py
@@ -8,5 +8,12 @@ setup(name='cab',
       author_email='moro.engelhardt@gmail.com',
       license='MIT',
       py_modules=['cabfile', 'cabinet', 'data', 'folder', 'header'],
-      package_dir={'': 'src'}
+      package_dir={'': 'src'},
+      classifiers=[
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+      ]
 )
